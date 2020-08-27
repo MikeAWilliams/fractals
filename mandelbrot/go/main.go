5b5357085a45d505f2654965ca6717628c663ebb
Mike Williams <mumakin@gmail.com>
2020-08-26 22:39:57 -0600

    messing with modules

--- mandelbrot/go/main.go
+++ mandelbrot/go/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	mandelbrot "github.com/MikeAWilliams/fractals/mandelbrot/go/mandelbrot/mandelbrot_lib"
+	"github.com/MikeAWilliams/fractals/mandelbrot/go/mandelbrot/mandelbrot_lib"
 )
 
 func main() {
 	fmt.Println("Starting Mandelbrot")
-	inSet, iter := mandelbrot.ComputeMandelbrot(30, 0, 1)
+	inSet, iter := mandelbrot_lib.ComputeMandelbrot(30, 0, 1)
 	fmt.Printf("inset=%v iter=%v\n", inSet, iter)
 	fmt.Println("Finished Mandelbrot")
 }
--- mandelbrot/go/test/mandelbrot_test.go
+++ mandelbrot/go/test/mandelbrot_test.go
@@ -3,11 +3,12 @@ package testMandelbrot
 import (
 	"testing"
 
+	"github.com/MikeAWilliams/fractals/mandelbrot/go/mandelbrot_lib"
 	"github.com/stretchr/testify/require"
 )
 
 func TEST_SOMETHING(t *testing.T) {
 
-	inSet, _ := mandelbrot.ComputeMandelbrot(30, 0, 1)
+	inSet, _ := mandelbrot_lib.ComputeMandelbrot(30, 0, 1)
 	require.True(t, inSet)
 }
