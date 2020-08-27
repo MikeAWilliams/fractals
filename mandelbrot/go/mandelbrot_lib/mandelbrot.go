998e84bc73a6278667dcadf0bd8574a72c9abacb
Mike Williams <mumakin@gmail.com>
2020-08-26 22:55:37 -0600

    mandelbrot tests pass

--- mandelbrot/go/mandelbrot_lib/mandelbrot.go
+++ mandelbrot/go/mandelbrot_lib/mandelbrot.go
@@ -1,23 +1,20 @@
 package mandelbrot_lib
 
-import "fmt"
-
 func ComputeMandelbrot(maxIterations int, c0Real, c0Im float64) (bool, int) {
-	//x2 := 0.0
-	//y2 := 0.0
-	//var iteration int
-	//	x := 0.0
-	//y := 0.0
+	var x2 float64
+	var y2 float64
+	var iteration int
+	var x float64
+	var y float64
 
-	//for ; iteration < maxIterations && x2+y2 < 4; iteration++ {
-	//y = 2*x*y + c0Im
-	//x = x2 - y2 + c0Real
-	//x2 = x * x
-	//y2 = y * y
-	//}
-	//if maxIterations == iteration {
-	//	return true, 0
-	//}
-	fmt.Println("in ComputeMandelbrot returning false 30")
-	return false, 30
+	for ; iteration < maxIterations && x2+y2 < 4; iteration++ {
+		y = 2*x*y + c0Im
+		x = x2 - y2 + c0Real
+		x2 = x * x
+		y2 = y * y
+	}
+	if maxIterations == iteration {
+		return true, 0
+	}
+	return false, iteration
 }
--- mandelbrot/go/test/mandelbrot_test.go
+++ mandelbrot/go/test/mandelbrot_test.go
@@ -11,4 +11,22 @@ func TestComputeMandelbrot(t *testing.T) {
 
 	inSet, _ := mandelbrot_lib.ComputeMandelbrot(30, 0, 1)
 	require.True(t, inSet)
+
+	insetNeg10, _ := mandelbrot_lib.ComputeMandelbrot(30, -1, 0)
+	require.True(t, insetNeg10)
+
+	inset05, _ := mandelbrot_lib.ComputeMandelbrot(30, 0, 0.5)
+	require.True(t, inset05)
+
+	inset10, iter10 := mandelbrot_lib.ComputeMandelbrot(30, 1, 0)
+	require.False(t, inset10)
+	require.Equal(t, 2, iter10)
+
+	inset02, iter02 := mandelbrot_lib.ComputeMandelbrot(30, 0, 2)
+	require.False(t, inset02)
+	require.Equal(t, 1, iter02)
+
+	inset55, iter55 := mandelbrot_lib.ComputeMandelbrot(30, 0.5, 0.5)
+	require.False(t, inset55)
+	require.Equal(t, 5, iter55)
 }
