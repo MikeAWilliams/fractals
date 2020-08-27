99605da859a8c8eabc3274277a8b5307acbcc364
MikeAWilliams <mumakin@gmail.com>
2020-08-26 20:01:38 -0600

    parallel implementation

--- mandelbrot/cpp/lib/mandelbrot.h
+++ mandelbrot/cpp/lib/mandelbrot.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <execution>
 #include <functional>
 #include <optional>
 #include <vector>
@@ -14,6 +15,13 @@ struct MandelbrotParameters
 };
 
 template<typename pixelType>
+struct foreachElement
+{
+    std::vector<pixelType>* result;
+    double imaginary;
+};
+
+template<typename pixelType>
 std::vector<std::vector<pixelType>> ComputeMandelbrot(
     const MandelbrotParameters params, 
     std::function<pixelType()> getInSetPixel, 
@@ -22,23 +30,29 @@ std::vector<std::vector<pixelType>> ComputeMandelbrot(
     const double realPixelSize {(params.realMax - params.realMin)/(params.sizeX - 1)};
     const double imaginaryPixelSize {(params.imMax - params.imMin)/(params.sizeY - 1)};
     std::vector<std::vector<pixelType>> result {params.sizeY};
+    
+    std::vector<foreachElement<pixelType>> tmpVec {params.sizeY};
     for(size_t yIndex {0}; yIndex < params.sizeY; ++yIndex)
     {
-        const double imaginary {params.imMin + yIndex * imaginaryPixelSize };
-
+        tmpVec[yIndex].result = &result[yIndex];
+        tmpVec[yIndex].imaginary= params.imMin + yIndex * imaginaryPixelSize;
+    }
+    std::for_each(std::execution::par, tmpVec.cbegin(), tmpVec.cend(), 
+        [&params, &getInSetPixel, &getOutOfSetPixel, &realPixelSize](auto& vecElem)
+    {
         for(size_t xIndex{0}; xIndex < params.sizeX; ++xIndex)
         {
             const double real {params.realMin + xIndex * realPixelSize};
-            const auto pointResult {ComputeMandelbrot(params.maxIterations, real, imaginary)};
+            const auto pointResult {ComputeMandelbrot(params.maxIterations, real, vecElem.imaginary)};
             if(pointResult.has_value())
             {
-                result[yIndex].emplace_back(getOutOfSetPixel(pointResult.value()));
+                vecElem.result->emplace_back(getOutOfSetPixel(pointResult.value()));
             }
             else
             {
-                result[yIndex].emplace_back(getInSetPixel());
+                vecElem.result->emplace_back(getInSetPixel());
             }
         }
-    }
+    });
     return result;
 }
