284b64f54c1ab61dd20810e760d089885790aae1
Mike Williams <mumakin@gmail.com>
2020-08-26 22:45:36 -0600

    tests finally fail

--- mandelbrot/go/test/mandelbrot_test.go
+++ mandelbrot/go/test/mandelbrot_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TEST_SOMETHING(t *testing.T) {
+func TestComputeMandelbrot(t *testing.T) {
 
 	inSet, _ := mandelbrot_lib.ComputeMandelbrot(30, 0, 1)
 	require.True(t, inSet)
