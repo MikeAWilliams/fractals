c44493e082f442f2ba7ed2ab5ba4de9e266052ff
Mike Williams <mumakin@gmail.com>
2020-08-26 21:54:32 -0600

    update files

--- mandelbrot/go/test/mandelbrot_test.go
+++ mandelbrot/go/test/mandelbrot_test.go
@@ -3,11 +3,12 @@ package testMandelbrot
 import (
 	"testing"
 
+	"github.com/MikeAWilliams/fractals/tree/master/mandelbrot/go/mandelbrot"
 	"github.com/stretchr/testify/require"
 )
 
 func TEST_SOMETHING(t *testing.T) {
 
-	inSet, iterations := ComputeMandelbrot(30, 0, 1)
+	inSet, iterations := mandelbrot.ComputeMandelbrot(30, 0, 1)
 	require.True(inSet)
 }
