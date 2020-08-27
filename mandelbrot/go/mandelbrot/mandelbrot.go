c79ef472682c7c867f351981f9cf20c1b3adcb6b
Mike Williams <mumakin@gmail.com>
2020-08-26 21:55:47 -0600

    fixing mandelbrot

--- mandelbrot/go/mandelbrot/mandelbrot.go
+++ mandelbrot/go/mandelbrot/mandelbrot.go
@@ -3,15 +3,15 @@ package mandelbrot
 func ComputeMandelbrot(maxIterations int, c0Real, c0Im float64) (bool, int) {
 	x2 := 0.0
 	y2 := 0.0
-    iteration := 0
-    x := 0.0
-    y := 0.0
+	iteration := 0
+	x := 0.0
+	y := 0.0
 
-    for(; iteration < maxIterations && x2 + y2 < 4; iteration++) {
-        y = 2 * x * y + c0Im;
-        x = x2 - y2 + c0Real;
-        x2 = x * x;
-        y2 = y * y;
+	for ; iteration < maxIterations && x2+y2 < 4; iteration++ {
+		y = 2*x*y + c0Im
+		x = x2 - y2 + c0Real
+		x2 = x * x
+		y2 = y * y
 	}
 	if maxIterations == iteration {
 		return true, 0
