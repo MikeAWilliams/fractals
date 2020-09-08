080b3e61e7d66899d319665462ae4ff8ebdde24d
Mike Williams <mumakin@gmail.com>
2020-09-07 20:45:16 -0600

    remove profiling

--- mandelbrot/go/main.go
+++ mandelbrot/go/main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"runtime/pprof"
-
 	"github.com/MikeAWilliams/fractals/mandelbrot/go/mandelbrot/mandelbrot_lib"
 )
-
-var cpuprofile = flag.String("cpuprofile", "cpu.prof", "write cpu profile to `file`")
 
 func main() {
 	params := mandelbrot_lib.Parameters{30, mandelbrot_lib.Point{-2.0, -1.0}, mandelbrot_lib.Point{1.0, 1.0}, mandelbrot_lib.Pixel{1920, 1080}}
@@ -17,17 +10,5 @@ func main() {
 	outColor := mandelbrot_lib.Color{255, 255, 255}
 	fileName := "out.png"
 
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
 	mandelbrot_lib.CreateColorMandelbrotNoPipeGoroutines(params, inSetColor, outColor, fileName)
 }
